37cb96cb2e9d94d556a9c926e9dae53c1221d44f
PHILLIP NGUYEN <PNguyen0619@slps.org>
2023-04-26 12:55:54 -0500

    Add project files.

---
+++ Wheel of Fortune Capstone Project/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Wheel of Fortune Capstone Project")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("SLPS")>
+<Assembly: AssemblyProduct("Wheel of Fortune Capstone Project")>
+<Assembly: AssemblyCopyright("Copyright © SLPS 2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6a5cf587-b508-4cd4-b003-f5bc45923c5d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
