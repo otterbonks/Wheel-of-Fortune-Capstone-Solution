bbdc63ec953389fab2fc0d0f1571efd4ac8f7cd0
PHILLIP NGUYEN <PNguyen0619@slps.org>
2023-04-27 08:08:27 -0500

    THE ANIMATION WORKS

--- Wheel of Fortune Capstone Project/Main Form.vb
+++ Wheel of Fortune Capstone Project/Main Form.vb
@@ -1,34 +1,37 @@
-﻿Option Strict On
-Option Explicit On
-Option Infer Off
-' Name:         Wheel of Fortune Capstone
+﻿' Name:         Wheel of Fortune Capstone
 ' Purpose:
 ' Programmers:  Phillip Nguyen and David Garrison on 4/25/2023
-Imports System.Threading
+
+Option Strict On
+Option Explicit On
+Option Infer Off
 
 Public Class frmMain
     Dim seconds As Integer
+    Dim wheelstate As Integer = 1
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         picWheel.Image = WheelImages.Images(0)
-        Timer1.Interval = 1000
+        Timer1.Interval = 50
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim wheelspin As Integer
         Timer1.Start()
 
     End Sub
 
-    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        Dim wheelstate As Integer = 1
+    Private Async Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         seconds += 1
 
         If seconds = 1 Then
-            picWheel.Image = WheelImages.Images(wheelstate)
-            wheelstate += 1
+            If wheelstate < 24 Then
+                picWheel.Image = WheelImages.Images(wheelstate)
+                wheelstate += 1
+            Else
+                wheelstate = 0
+                picWheel.Image = WheelImages.Images(wheelstate)
+            End If
+            seconds = 0
         End If
-        seconds = 0
-
-            Timer1.Stop()
     End Sub
 End Class
